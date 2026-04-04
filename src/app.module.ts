4eff4d3a40dcdb7f9384c5bdf9650f7254688e07
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-04 11:43:03 +0600

    category folder created is done

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { ProductService } from './product/product.service';
+import { ProductController } from './product/product.controller';
+import { EmployeeModule } from './employee/employee.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
+  imports: [EmployeeModule, CategoryModule],
+  controllers: [AppController, UserController, ProductController],
   providers: [AppService, ProductService],
 })
 export class AppModule {}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,5 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProductService {
-    
+    private products = [
+        {id: 1 , name: "Mobile" , price: 2000},
+        {id: 2 , name: "Teblet" , price: 4000},
+        {id: 3 , name: "Laptop" , price: 80000}
+    ]
+
+    getAllProducts() {
+        return this.products;
+    }
+    getProductById(id: number) {
+        return this.products.find((product) => product.id === id);
+    }
 }

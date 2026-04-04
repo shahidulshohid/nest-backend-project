530c8d65a712ccfc882829c506dade77af49f589
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-04 18:56:06 +0600

    class validator and class transformer install is done

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,4 +1,10 @@
+// i have to install here class validator class-transformer 
+
+import { IsInt, IsString } from "class-validator";
+
 export class CreateCustomerDto {
+    @IsString()
     name: string;
+    @IsInt()
     age: number;
 }

95a01636935b6d87ba563e268c4c1869c0bac074
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-04 17:48:25 +0600

    create customer created is done

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+    constructor(private readonly customerService: CustomerService){}
+
+    @Get()
+    getCustomers(){
+        return this.customerService.getAllCustomers();
+    }
+
+    @Post()
+    addCustomer(@Body () createcustomerDto: CreateCustomerDto){
+        return this.customerService.addCustomer(createcustomerDto);
+    }
+
+}
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Customer } from './interfaces/customer.interface';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+    private customer: Customer[] = [];
+
+    getAllCustomers(): Customer[] {
+        return this.customer;
+    }
+
+    addCustomer(createCustomerDto: CreateCustomerDto): Customer {
+        const newCustomer: Customer = {
+            id: Date.now(),
+            ...createCustomerDto
+        };
+
+        this.customer.push(newCustomer);
+        return newCustomer;
+    }
+}

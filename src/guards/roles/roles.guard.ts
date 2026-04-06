c4bb39f10bce0c8f726f2a45de179ffb9a803813
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-06 14:32:32 +0600

    roles guard is impelement is done

--- src/guards/roles/roles.guard.ts
+++ src/guards/roles/roles.guard.ts
@@ -1,11 +1,26 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { Role } from './roles.enums';
+import { ROLES_KEY } from './roles.decorator';
+import { stringify } from 'querystring';
+import { request } from 'http';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) { }
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+      ROLES_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]
+    )
+    if (!requiredRoles) return true;
+    const request = context.switchToHttp().getRequest<{ headers: Record<string, string>}>();  
+   const userRole = request.headers['x-user-role'] as Role;
+   return requiredRoles.includes(userRole);
   }
 }

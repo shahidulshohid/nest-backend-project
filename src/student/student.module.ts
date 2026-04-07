78085456e70de377126140b56fd3087ac825af27
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-07 15:31:55 +0600

    student schema file created is done

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Student, StudentSchema } from './student.schema';
 
 @Module({
   controllers: [StudentController],
-  providers: [StudentService]
+  providers: [StudentService],
+  imports: [
+    MongooseModule.forFeature([{name: Student.name, schema: StudentSchema }])
+  ]
 })
 export class StudentModule {}

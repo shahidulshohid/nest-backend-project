b92aa95ed711c3a0883e0ce7b810d4d30425a78b
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-09 16:30:33 +0600

    address schema and user schema functionality implement is done

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user.controller';
+import { User, UserSchema } from './schemas/user.schema';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
+  providers: [UserService],
+  controllers: [UserController]
+})
 export class UserModule {}

480b01b97c41cb9b0156e3231c69fddb12fb0c57
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-09 17:25:29 +0600

    user service file created is done

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+    
+    async createUser(): Promise<User> {
+        const user = new this.userModel({
+            name: "john doe",
+            address: {
+                street: "123 street",
+                city: "karachi"
+            }
+        })
+        return user.save();
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.userModel.find().exec();
+    }
+
+}

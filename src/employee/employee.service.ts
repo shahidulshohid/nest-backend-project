72a1641ef1d92f76744576cc8e3c837321799da2
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-10 11:49:59 +0600

    employee service created is done

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Employee } from './schemas/employee.schema';
+import { Profile } from './schemas/profile.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+    constructor(
+        @InjectModel(Employee.name) private employeeModel: Model<Employee>,
+        @InjectModel(Profile.name) private profileModel: Model<Profile>
+    ) { }
+
+    async createEmployee(): Promise<Employee> {
+        const profile = await new this.profileModel({
+            age: 25,
+            qualification: 'Masters'
+        }).save();
+        const employee = new this.employeeModel({
+            name: 'Farzeen',
+            profile: profile._id
+        })
+        return employee.save();
+    }
+
+    async findAll(): Promise<Employee[]> {
+        return this.employeeModel.find().populate('profile').exec();
+    }
+}

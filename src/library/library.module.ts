8ba6955e31f4a185f9e661b15fd526ca5dc3e40c
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-13 10:31:02 +0600

    book schema file created is done

--- src/library/library.module.ts
+++ src/library/library.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { LibraryController } from './library.controller';
+import { LibraryService } from './library.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Library, LibrarySchema } from './schemas/library.schema';
+import { Book, BookSchema } from './schemas/book.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {name:Library.name, schema: LibrarySchema},
+      {name:Book.name, schema: BookSchema},
+    ])
+  ],
+  controllers: [LibraryController],
+  providers: [LibraryService]
+})
 export class LibraryModule {}
---
+++ src/library/schemas/library.schema.ts
@@ -0,0 +1,13 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+
+@Schema()
+export class Library extends Document {
+    @Prop()
+    name: string;
+
+    @Prop({type: [{type: Types.ObjectId, ref: 'Book'}]})
+    books: Types.ObjectId[];
+}
+
+export const LibrarySchema = SchemaFactory.createForClass(Library);

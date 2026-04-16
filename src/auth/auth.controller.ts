ae47d390fcab244fe41cd37e3b4a35da191147a3
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-16 11:37:59 +0600

    auth controller file created is done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    async signup(@Body() body: {email: string, password: string}){
+        return this.authService.signup(body.email, body.password)
+    }
+
+    @Post('login')
+    async login(@Body() body: {email: string, password: string}){
+        return this.authService.login(body.email, body.password)
+    }
+}

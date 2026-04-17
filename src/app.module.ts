48dfe062ff0d66fb00c8df647d071a357440de49
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-17 15:19:37 +0600

    create nest js file structure is done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { EmployeesModule } from './employees/employees.module';
-import { AuthModule } from './auth/auth.module';
-
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    UserModule,
-    EmployeesModule,
-    AuthModule
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })

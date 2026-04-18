bcb4df0670db73172345a265500a2d34505df4a2
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-18 14:51:36 +0600

    book resoler file functionality implement is done

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,4 +1,36 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Book } from './model/book.model';
+import { BookService } from './book.service';
+import { CreateBookInput } from './dto/create-book.input';
+import { UpdateBookInput } from './dto/update-book.input';
 
-@Resolver()
-export class BookResolver {}
+@Resolver(() => Book)
+export class BookResolver {
+    constructor(private readonly bookService: BookService) {}
+
+    @Query(() => [Book]) 
+    GetAllBooks() {
+        return this.bookService.findAll();
+    }
+
+    @Query(() => Book)
+    GetBook(@Args('id') id: string) {
+        return this.bookService.findOne(id);
+    }
+
+    @Mutation(() => Book) 
+    createBook(@Args('input') input: CreateBookInput) {
+        return this.bookService.create(input);
+    }
+
+    @Mutation(() => Book) 
+    updateBook(@Args('input') input: UpdateBookInput) {
+        return this.bookService.update(input);
+    }
+
+    @Mutation(() => Book) 
+    deleteBook(@Args('id') id: string) {
+        return this.bookService.remove(id);
+    }
+
+}

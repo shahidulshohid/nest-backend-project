4a08ecf5bf05cfeb5757fafcd7214177de9627d3
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-18 11:02:36 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,11 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+
+
+// npm i @nestjs/graphql @nestjs/apollo graphql apollo-server-express 
+// npm i prisma --save-dev
+// npm i @prisma/client
+//npx prisma init // after install this create .env file and add DATABASE_URL="your database url" in .env file
+// for ORM will use prisma ORM and for database will use NEON DB 

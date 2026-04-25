c88bc698186bd043d7cf6fa259f87a467322d3d8
shahidul islam <shahidulislamshohid7@gmail.com>
2026-04-25 09:26:52 +0600

    prisma migration is done

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-<<<<<<< HEAD
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-=======
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
->>>>>>> c9cf6c9840589dfc08a22448052b99514395bb5f
+  app.useGlobalPipes(new ValidationPipe()); 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
